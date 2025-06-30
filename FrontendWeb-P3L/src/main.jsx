e7f6986dd3121c06e05ce7ffd8c6b36689952686
NightOwlLab <ivantjandra18@gmail.com>
2025-06-30 17:54:58 +0700

    [FIX] add bootstrap

--- FrontendWeb-P3L/src/main.jsx
+++ FrontendWeb-P3L/src/main.jsx
@@ -4,6 +4,9 @@ import AppRouter from "./routes/index";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+import * as bootstrap from "bootstrap";
+window.bootstrap = bootstrap;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppRouter />
